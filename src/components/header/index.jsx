76dae632a293e054c1c678cac37265d71dc7fefe
Gustavo <gmalmeida26@gmail.com>
2023-08-16 20:58:07 -0300

    feat: header component created

---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+function Header () {
+    return (
+        <>
+        <li>Register Pharmacy</li>
+        <li>Register Medicine</li>
+        <li>Registered Medicines</li>
+        <li>Find Pharmacy</li>
+        </>
+    )
+}
+
+export { Header }
